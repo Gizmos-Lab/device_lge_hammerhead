92866be31411dca99b88fc7da6b172046eab5c57
Kevin Rocard <krocard@google.com>
2017-08-17 12:14:53 -0700

    hammerhead: Convert audio_effects.conf to XML
    Converted .conf format to .xml using:
    https://github.com/luk1337/aeffects-conf2xml
    
    Bug: 37492580
    Test: dumped the effectFactory state and check it was unchanged
          $ adb shell dumpsys media.audio_flinger | sed '/Clients:/q'
    Change-Id: I233a44c2ba0cb4c4d4fb8c3720fc09862ce79827
    Merged-In: I233a44c2ba0cb4c4d4fb8c3720fc09862ce79827
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- device.mk
+++ device.mk
@@ -177,7 +177,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
 
-    device/lge/hammerhead/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/lge/hammerhead/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     device/lge/hammerhead/audio/audio_policy.conf:system/etc/audio_policy.conf \
     device/lge/hammerhead/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/lge/hammerhead/audio/mixer_paths.xml:system/etc/mixer_paths.xml
