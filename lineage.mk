03432438c25199e38c77fc46ee9396e8717a20f9
Xaif <huzaifaamirka46@gmail.com>
2018-01-07 15:49:06 +0530

    aosip time

--- lineage.mk
+++ lineage.mk
@@ -3,8 +3,8 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Aosip stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
@@ -14,7 +14,7 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/hammerhead/overlay/cm
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := lineage_hammerhead
+PRODUCT_NAME := aosip_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
